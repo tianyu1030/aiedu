8ea912f737095f09ea2b6c45f66986c053a98b45
Scorpio <18556810300@163.com>
2026-08-25 15:26:58 +0800

    bugfix

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -10,7 +10,7 @@ export default function AppLayout({
 }) {
   return (
     <SidebarProvider>
-      <div className="min-h-screen bg-bg text-fg" themed>
+      <div className="min-h-screen bg-bg text-fg" data-themed>
         <Sidebar />
         <NavBar />
         <MainShifter>
